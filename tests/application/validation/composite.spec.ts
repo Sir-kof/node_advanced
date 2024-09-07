96c355657b324cdad0c6319257e3d9800daad3ad
Rodrigo Fernandes <rodrigo_mdg_fernandes@outlook.com>
2024-09-07 20:07:55 -0300

    isolate sut creation

--- tests/application/validation/composite.spec.ts
+++ tests/application/validation/composite.spec.ts
@@ -13,14 +13,23 @@ class ValidationComposite {
 }
 
 describe('ValidationComposite', () => {
-  test('should return undefined if all Validators return undefined', () => {
-    const validator1 = mock<Validator>()
-    validator1.validate.mockReturnValue(undefined)
-    const validator2 = mock<Validator>()
-    validator2.validate.mockReturnValue(undefined)
-    const validators = [validator1, validator2]
-    const sut = new ValidationComposite(validators)
+  let sut: ValidationComposite
+  let validator1: MockProxy<Validator>
+  let validator2: MockProxy<Validator>
+  let validators: Validator[]
 
+  beforeAll(() => {
+    validator1 = mock<Validator>()
+    validator1.validate.mockReturnValue(undefined)
+    validator2 = mock<Validator>()
+    validator2.validate.mockReturnValue(undefined)
+    validators = [validator1, validator2]
+  })
+
+  beforeEach(() => {
+    sut = new ValidationComposite(validators)
+  })
+  test('should return undefined if all Validators return undefined', () => {
     const error = sut.validate()
 
     expect(error).toBeUndefined()
