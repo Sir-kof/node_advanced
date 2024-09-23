d46979a0ff84d595f68c6642e6f3d748c3b9fcd4
Rodrigo Fernandes <rodrigo_mdg_fernandes@outlook.com>
2024-09-23 11:22:01 -0300

    ensure AllowedMimeTypes returns undefined if value is valid

--- tests/application/validation/allowed_mime_types.spec.ts
+++ tests/application/validation/allowed_mime_types.spec.ts
@@ -5,8 +5,19 @@ type Extension = 'png' | 'jpg'
 export class AllowedMimeTypes {
   constructor (private readonly allowed: Extension[], private readonly mimeType: string) {}
 
-  validate (): Error {
-    return new InvalidMimeTypeError(this.allowed)
+  validate (): Error | undefined {
+    let isValid = false
+    if (this.isPng()) isValid = true
+    else if (this.isJpg()) isValid = true
+    if (!isValid) return new InvalidMimeTypeError(this.allowed)
+  }
+
+  private isPng (): boolean {
+    return this.allowed.includes('png') && this.mimeType === 'image/png'
+  }
+
+  private isJpg (): boolean {
+    return this.allowed.includes('jpg') && /\image\/jpe?g/.test(this.mimeType)
   }
 }
 
@@ -17,5 +28,29 @@ describe('AllowedMimeTypes', () => {
     const error = sut.validate()
 
     expect(error).toEqual(new InvalidMimeTypeError(['png']))
+  })
+
+  test('should return undefined if value is valid', () => {
+    const sut = new AllowedMimeTypes(['png'], 'image/png')
+
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
+  })
+
+  test('should return undefined if value is valid', () => {
+    const sut = new AllowedMimeTypes(['jpg'], 'image/jpg')
+
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
+  })
+
+  test('should return undefined if value is valid', () => {
+    const sut = new AllowedMimeTypes(['jpg'], 'image/jpeg')
+
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
   })
 })
