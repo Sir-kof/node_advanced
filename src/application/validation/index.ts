600a3ec58fb84c8be588022214e838130b3eebac
Rodrigo Fernandes <rodrigo_mdg_fernandes@outlook.com>
2024-09-23 11:33:26 -0300

    add MaxFileSize validator

--- src/application/validation/index.ts
+++ src/application/validation/index.ts
@@ -1,5 +1,6 @@
 export * from './validator'
 export * from './allowed_mime_types'
+export * from './max_file_size'
 export * from './composite'
 export * from './builder'
 export * from './required'
